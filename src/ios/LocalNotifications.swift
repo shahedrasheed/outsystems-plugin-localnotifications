baa040ad674e2ea11861f0ab932cdd6ba95b3c86
shahedrasheed <shahed.rasheed@gmail.com>
2025-06-16 13:46:43 +0300

    Update LocalNotifications.swift

--- src/ios/LocalNotifications.swift
+++ src/ios/LocalNotifications.swift
@@ -1,17 +1,31 @@
 /********* outsystems-plugin-localnotifications.m Cordova Plugin Implementation *******/
 import Foundation
-
+import UserNotifications
+ 
 @objc(LocalNotifications)
 class LocalNotifications: CDVPlugin {
-    
-    @objc(setLocalNotification:)
-    func setLocalNotification(command: CDVInvokedUrlCommand) {
+    @objc(setLocalNotifications:)
+    func setLocalNotifications(command: CDVInvokedUrlCommand) {
         print("Cool method called")
-        
-        sendPluginResult(status: .ok, message: "Everything went ok", callbackId: command.callbackId)
+        sendPluginResult(status: .ok, message: "All good", callbackId: command.callbackId)
     }
-    
-	//Generic function to be included in all plugins
+    @objc(checkPermissions:)
+    func checkPermissions(command: CDVInvokedUrlCommand) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                self.sendPluginResult(status: .error, message: error!.localizedDescription, callbackId: command.callbackId)
+            } else {
+                if success {
+                    print("All set!")
+                    self.sendPluginResult(status: .ok, message: "Permission IS allowed!", callbackId: command.callbackId)
+                } else {
+                    print("Permission not allowed!")
+                    self.sendPluginResult(status: .ok, message: "Permission NOT allowed!", callbackId: command.callbackId)
+                }
+            }
+        }
+    }
     func sendPluginResult(status: CDVCommandStatus, message: String = "", callbackId: String, keepCallback: Bool = false) {
         let pluginResult = CDVPluginResult(status: status, messageAs: message)
         pluginResult?.setKeepCallbackAs(keepCallback)
